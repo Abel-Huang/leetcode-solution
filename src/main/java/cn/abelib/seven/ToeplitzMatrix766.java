9a5550c34c752883266163d2d55a4320d29d654e
Abel-Huang <497720975@qq.com>
2019-01-07 01:36:12 +0800

    add 485 628 697

---
+++ src/main/java/cn/abelib/seven/ToeplitzMatrix766.java
@@ -0,0 +1,18 @@
+package cn.abelib.seven;
+
+import org.junit.Test;
+
+/**
+ * @Author: abel.huang
+ * @Date: 2019-01-06 05:09
+ */
+public class ToeplitzMatrix766 {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        return false;
+    }
+
+    @Test
+    public void isToeplitzMatrixTest(){
+
+    }
+}
