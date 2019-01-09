2afbd2df02bf99b914566adac8a3f96e5381a53d
Abel-Huang <497720975@qq.com>
2019-01-10 00:51:44 +0800

    add 167 830

---
+++ src/main/java/cn/abelib/one/TwoSumSortedInputArray167.java
@@ -0,0 +1,34 @@
+package cn.abelib.one;
+
+import org.junit.Test;
+
+/**
+ * @Author: abel.huang
+ * @Date: 2019-01-09 21:31
+ */
+public class TwoSumSortedInputArray167 {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        if (numbers.length < 2) {
+            return result;
+        }
+        for (int i = 0; i < numbers.length ; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    public void twoSum(){
+        int[] numbers = {0, 0, 3, 4};
+        int target = 0;
+        System.out.println(twoSum(numbers, target)[0] + " " + twoSum(numbers, target)[1]);
+    }
+}
